465d6bb3258eeb6ac6bd94031a735d3b37f8409e
Aydash16 <oaidash2000@gmail.com>
2023-12-05 19:02:40 +0700

    sfd

---
+++ 5 labs/second/second/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace first;
+
+public static class Program
+{
+    public static void Main()
+    {
+        string path = "C:/Users/gr622_orahe/Desktop/rkis/5 labs/numsTask2.txt";
+        using StreamReader reader = new StreamReader(path);
+
+        string str = reader.ReadToEnd();
+        
+        reader.Close();
+        
+        using StreamWriter writer = new StreamWriter(path);
+
+        writer.Write(String.Empty);
+
+        double[] nums = str.Split(';').Select(double.Parse).ToArray();
+
+        double temp;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+             for (int j = i; j < nums.Length - 1; j++)
+            {
+                if(nums[j] > nums[j + 1])
+                {
+                    temp = nums[j];
+                    nums[j] = nums[j + 1];
+                    nums[j + 1] = temp;
+                }
+            }
+        }
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            writer.Write(nums[i] + ';');
+            Console.WriteLine(nums[i]);
+        }
+        writer.Close();
+    }
+}
